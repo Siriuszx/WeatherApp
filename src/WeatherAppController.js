23f0b0171272b17e2026b81e27a04c37e4565743
Siriusxz <114768817+Siriuszx@users.noreply.github.com>
2024-02-10 23:18:13 +0100

    Sync

--- src/WeatherAppController.js
+++ src/WeatherAppController.js
@@ -1,24 +1,23 @@
-import WeatherDataHandler from "./WeatherDataHandler";
-import WeatherAppStorage from "./WeatherAppStorage";
-import WeatherAppUI from "./WeatherAppUI";
+import WeatherDataHandler from './WeatherDataHandler';
+import WeatherAppStorage from './WeatherAppStorage';
+import WeatherAppUI from './WeatherAppUI';
 
 class WeatherAppController {
+  constructor() {
+    this.APIHandler = new WeatherDataHandler();
+    this.UIController = new WeatherAppUI({
+      getForecast: this.getForecast.bind(this),
+    });
+  }
 
-    constructor() {
-        this.APIHandler = new WeatherDataHandler();
-        this.UIController = new WeatherAppUI(
-            {
-                getForecast: this.getForecast.bind(this)
-            }
-        );
-    }
+  async getForecast() {
+    const weatherData = await this.APIHandler.getLocationWeatherData(
+      this.UIController.getLocation() || 'Berlin'
+    );
 
-    async getForecast() {
-        const weatherData = await this.APIHandler.getLocationWeatherData(this.UIController.getLocation() || 'Berlin');
-
-        this.UIController.logCurrentWeather(weatherData);
-        this.UIController.updateCurrentTemperature(weatherData);
-    }
+    this.UIController.logCurrentWeather(weatherData);
+    this.UIController.updateCurrentTemperature(weatherData);
+  }
 }
 
 export default WeatherAppController;
